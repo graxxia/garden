7c4f5e21a8b268544017505a9925c6ca0af39155
gracia <graxxia@hotmail.com>
2020-06-03 00:17:19 -0400

    Front End
    Svelte, Material

--- front/src/App.svelte
+++ front/src/App.svelte
@@ -1,11 +1,14 @@
 <script>
   export let name;
+  import Button from "@smui/button";
+  let clicked = 1;
 </script>
 
 <style>
   h1 {
-    color: rgb(97, 204, 35);
+    color: purple;
   }
 </style>
 
 <h1>Hello {name}!</h1>
+<Button>{(clicked += 1)}</Button>
