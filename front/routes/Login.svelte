a80e3da9f11939955d11b5b1749c2b98f023028e
gracia <graxxia@hotmail.com>
2020-06-13 00:01:29 -0400

    Added cookie

--- front/routes/Login.svelte
+++ front/routes/Login.svelte
@@ -2,76 +2,82 @@
   import Button from "@smui/button";
   import Tab, { Icon, Label } from "@smui/tab";
   import Textfield from "@smui/textfield";
+  import { setCookie } from "../src/cookie.js";
+  import router from "page";
 
- let error_boolean = false;
-   let username = "";
+  let error_boolean = false;
+  let username = "";
   //   export let Username = "";
-   let password = "";
+  let password = "";
   //   export let comment = "";
 
-    const apiUrl = "http://localhost:5000/users/authenticate";
-//
-async function postData(url = '', data = {}) {
-  // Default options are marked with *
-  const response = await fetch(url, {
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    mode: 'cors', // no-cors, *cors, same-origin
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'same-origin', // include, *same-origin, omit
-    headers: {
-      'Content-Type': 'application/json'
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    redirect: 'follow', // manual, *follow, error
-    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-    body: JSON.stringify(data) // body data type must match "Content-Type" header
-  });
-  return response.json(); // parses JSON response into native JavaScript objects
-}
+  const apiUrl = "http://localhost:5000/users/authenticate";
+  //
+  async function postData(url = "", data = {}) {
+    // Default options are marked with *
+    const response = await fetch(url, {
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      mode: "cors", // no-cors, *cors, same-origin
+      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: "same-origin", // include, *same-origin, omit
+      headers: {
+        "Content-Type": "application/json"
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: "follow", // manual, *follow, error
+      referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      body: JSON.stringify(data) // body data type must match "Content-Type" header
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
+  }
 
+  //
+  async function handleSubmit(event) {
+    console.log(event);
+    console.log(event.target);
+    console.log(event.target.username.value);
+    console.log(event.target.password.value);
 
-//
-   async function handleSubmit(event) {
-        console.log(event);
-        console.log(event.target);
-        console.log(event.target.username.value);
-        console.log(event.target.password.value);
-
-        postData(apiUrl, { username: event.target.username.value, password: event.target.password.value })
-  .then(data => {
-    console.log(data); // JSON data parsed by `response.json()` call
-  });
+    const userData = await postData(apiUrl, {
+      username: event.target.username.value,
+      password: event.target.password.value
+    });
+    setCookie(
+      "user-token",
+      JSON.stringify({ username: userData.username, token: userData.token }),
+      7
+    );
+    router.redirect("/");
+  }
+  function validateMessageUsername(event) {
+    let textbox = event.target;
+    error_boolean = false;
+    if (textbox.value === "") {
+      textbox.setCustomValidity("Required username");
+    } else if (textbox.validity.typeMismatch) {
+      error_boolean = true;
+      textbox.setCustomValidity("please enter a valid username");
+    } else {
+      textbox.setCustomValidity("");
     }
-   function validateMessageUsername(event) {
-       let textbox = event.target;
-       error_boolean = false;
-       if (textbox.value === '') {
-            textbox.setCustomValidity('Required username');
-        } else if (textbox.validity.typeMismatch){
-            error_boolean = true;
-            textbox.setCustomValidity('please enter a valid username');
-        } else {
-           textbox.setCustomValidity('');
-        }
-        return true;
-    }
-
+    return true;
+  }
 </script>
 
 <form
-    on:submit|preventDefault="{handleSubmit}"
-    on:invalid={validateMessageUsername}
-    on:changed={validateMessageUsername}
-    on:input={validateMessageUsername}
->
+  on:submit={handleSubmit}
+  on:invalid={validateMessageUsername}
+  on:changed={validateMessageUsername}
+  on:input={validateMessageUsername}>
   <label for="username">Username</label>
   <input required type="username" id="username" />
   {#if error_boolean}
-    <h1> OH NO! AN ERRROR!</h1>
+    <h1>OH NO! AN ERRROR!</h1>
   {/if}
 
   <label for="password">Password</label>
   <input required type="password" id="password" />
 
   <button type="submit">Login to account</button>
+  <a href="/register">Register</a>
 </form>
--- front/src/App.svelte
+++ front/src/App.svelte
@@ -1,16 +1,28 @@
 <script>
+  import Register from "./../routes/Register.svelte";
   import router from "page";
   import Home from "../routes/Home.svelte";
   import Blog from "../routes/Blog.svelte";
   import Login from "../routes/Login.svelte";
-  import TabBar from "@smui/tab-bar";
-  import Button from "@smui/button";
-  import Tab, { Icon, Label } from "@smui/tab";
   import SingleBlog from "../routes/SingleBlog.svelte";
+  import { MDCTabBar } from "@material/tab-bar";
+  import { onMount } from "svelte";
+  import { checkCookie, deleteCookie } from "../src/cookie";
 
   let active = "Home";
   let page;
   let params;
+  let src = "gardenialogo.svg";
+  let loggedIn = false;
+  let username;
+  let token;
+
+  onMount(async () => {
+    const tabBar = await new MDCTabBar(document.querySelector(".mdc-tab-bar"));
+    if (checkCookie("user-token")) {
+      loggedIn = true;
+    }
+  });
 
   router("/", () => (page = Home));
   router("/blog", () => (page = Blog));
@@ -27,46 +39,103 @@
     () => (page = SingleBlog)
   );
   router("/login", () => (page = Login));
+
+  router("/register", () => (page = Register));
+
   router.start();
+
+  //document.cookie = "cookiename=cookievalue";
+  //console.log(document.cookie);
 </script>
 
 <style>
-  h1 {
-    color: purple;
+  .flex-nav {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    align-content: center;
   }
-  section > div {
-    margin-bottom: 40px;
+
+  img {
+    width: 400px;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
   }
 </style>
 
-<h1>my Garden</h1>
+<svelte:component this={page} />
+<svelte:head>
+  <link
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+</svelte:head>
 
+<img {src} alt="Gardenia Logo" />
 <nav>
   <a href="/">Home</a>
   <a href="/blog">Blog</a>
-  <a href="/login">Login</a>
+  {#if !loggedIn}
+    <a href="/login">Login</a>
+    <a href="/register">Register</a>
+  {/if}
+  {#if loggedIn}
+    <button type="submit" on:click={() => deleteCookie('user-token', '', -1)}>
+      Sign out
+    </button>
+  {/if}
+
 </nav>
 
 <main>
   <svelte:component this={page} {params} />
 </main>
 
-<div>
-  <TabBar tabs={['Home', 'Blog', 'About Us']} let:tab bind:active>
-    <!-- Notice that the `tab` property is required! -->
-    <Tab {tab}>
-      <Label>{tab}</Label>
-    </Tab>
-  </TabBar>
-
-  <div style="margin-top: 15px;">
-    Programmatically select:
-    {#each ['Home', 'Blog', 'About Us'] as tab}
-      <Button on:click={() => (active = tab)}>
-        <Label>{tab}</Label>
-      </Button>
-    {/each}
+<div class="mdc-tab-bar" role="tablist">
+  <div class="mdc-tab-scroller">
+    <div class="mdc-tab-scroller__scroll-area">
+      <div class="mdc-tab-scroller__scroll-content">
+        <div class="flex-nav">
+          <button
+            class="mdc-tab mdc-tab--active"
+            role="tab"
+            aria-selected="true"
+            tabindex="0">
+            <span class="mdc-tab__content">
+              <span class="mdc-tab__icon material-icons" aria-hidden="true">
+                favorite
+              </span>
+              <span class="mdc-tab__text-label">Plant</span>
+            </span>
+            <span class="mdc-tab-indicator mdc-tab-indicator--active">
+              <span
+                class="mdc-tab-indicator__content
+                mdc-tab-indicator__content--underline" />
+            </span>
+            <span class="mdc-tab__ripple" />
+          </button>
+          <button
+            class="mdc-tab mdc-tab--active"
+            role="tab"
+            aria-selected="true"
+            tabindex="0">
+            <span class="mdc-tab__content">
+              <span class="mdc-tab__icon material-icons" aria-hidden="true">
+                favorite
+              </span>
+              <span class="mdc-tab__text-label">Login</span>
+            </span>
+            <span class="mdc-tab-indicator mdc-tab-indicator--active">
+              <span
+                class="mdc-tab-indicator__content
+                mdc-tab-indicator__content--underline" />
+            </span>
+            <span class="mdc-tab__ripple" />
+          </button>
+        </div>
+      </div>
+    </div>
   </div>
-
-  <pre class="status">Selected: {active}</pre>
 </div>
